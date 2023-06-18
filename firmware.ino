f38c9817a7e5a0cccba6d66f8434e850d55347a9
ypc2e55orj <ypc2e55orj@gmail.com>
2023-06-18 12:00:43 +0900

    ブザーを鳴らす

--- firmware.ino
+++ firmware.ino
@@ -1,13 +1,30 @@
 #include "src/driver/encoder.h"
 #include "src/driver/imu.h"
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   driver::encoder::init();
   driver::imu::init();
+
+  pinMode(21, OUTPUT);
+  digitalWrite(21, LOW);
+  buzzer();
 }
 
-void loop() {
+void buzzer()
+{
+  for (int i = 0; i < 100; i++)
+  {
+    digitalWrite(21, HIGH);
+    delayMicroseconds(500);
+    digitalWrite(21, LOW);
+    delayMicroseconds(500);
+  }
+}
+
+void loop()
+{
   uint16_t left, right;
   driver::encoder::angle(left, right);
   Serial.printf("\r\n%d, %d", left, right);
