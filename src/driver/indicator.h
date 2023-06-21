6715984eb70783cf2abbe053eea80eebc30e2555
ypc2e55orj <ypc2e55orj@gmail.com>
2023-06-21 14:45:47 +0900

    改行

--- src/driver/indicator.h
+++ src/driver/indicator.h
@@ -5,9 +5,11 @@
 namespace driver::indicator
 {
   void init();
+
   void set(uint8_t pos, uint8_t r, uint8_t g, uint8_t b);
   void set(uint8_t pos, uint32_t rgb);
   void clear();
+
   void show();
 
   uint8_t num();
