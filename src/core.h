1343e31fa0314706620def608e30752f06d5d7cf
ypc2e55orj <ypc2e55orj@gmail.com>
2023-06-27 14:21:35 +0900

    タスク設定用

---
+++ src/core.h
@@ -0,0 +1,9 @@
+#pragma once
+
+enum
+{
+  CORE_SENSE = 0,
+  CORE_CTRL = 0,
+  CORE_LOG = 1,
+  CORE_MAIN = 1
+};
---
+++ src/priority.h
@@ -0,0 +1,9 @@
+#pragma once
+
+enum
+{
+  PRIORITY_HIGH = 0,
+  PRIORITY_NORMAL = 5,
+  PRIORITY_LOW = 10,
+
+};
