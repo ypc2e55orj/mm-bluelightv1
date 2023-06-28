c89a7d94e42cceef501358edf4f06d2ebd1baac4
ypc2e55orj <ypc2e55orj@gmail.com>
2023-06-28 18:34:56 +0900

    Revert "インジケータの制御方法を変える"
    This reverts commit 50f9954a00f5c477d89c9344130a5c1ee771d073.

--- main/main.cpp
+++ main/main.cpp
@@ -2,7 +2,6 @@
 #include <freertos/FreeRTOS.h>
 #include <freertos/event_groups.h>
 #include <freertos/task.h>
-#include <esp_timer.h>
 
 #include <iostream>
 #include <cassert>
@@ -12,29 +11,23 @@
 #include "../src/driver/indicator.h"
 #include "../src/driver/buzzer.h"
 
-#include "../src/core.h"
-#include "../src/priority.h"
-
 #define EVENT_GROUP_SENSOR_IMU (1 << 1)
 #define EVENT_GROUP_SENSOR_ENCODER (1 << 2)
 #define EVENT_GROUP_SENSOR_ALL (EVENT_GROUP_SENSOR_IMU | EVENT_GROUP_SENSOR_ENCODER)
 
-uint32_t diff = 0;
 EventGroupHandle_t xEventGroupSensor = nullptr;
 
-void sensorTask(void *pv)
+void sensorTask(void *unused)
 {
   while (true)
   {
-    int64_t start = esp_timer_get_time();
     driver::imu::update();
     driver::encoder::update();
     vTaskDelay(pdMS_TO_TICKS(10));
-    diff = esp_timer_get_time() - start;
   }
 }
 
-void mainTask(void *pv)
+void mainTask(void *unused)
 {
   for (int i = 0; i < driver::indicator::nums(); i++)
   {
@@ -61,16 +54,15 @@ void mainTask(void *pv)
       auto [accel_x, accel_y, accel_z] = driver::imu::accel();
 
       std::cout << "\x1b[2J\x1b[0;0H"
-                << "Diff             : " << diff << std::endl
-                << "Encoder Left     : " << angle_left << std::endl
-                << "Encoder Right    : " << angle_right << std::endl
-                << "Gyro  X [dgree/s]: " << gyro_x << std::endl
-                << "Gyro  Y [dgree/s]: " << gyro_y << std::endl
-                << "Gyro  Z [dgree/s]: " << gyro_z << std::endl
-                << "Accel X   [m/s^2]: " << accel_x << std::endl
-                << "Accel Y   [m/s^2]: " << accel_y << std::endl
-                << "Accel Z   [m/s^2]: " << accel_z << std::endl
-                << std::endl;
+                << "Left    : " << angle_left << std::endl
+                << "Right   : " << angle_right << std::endl
+                << "Gyro  X [mdps]: " << gyro_x << std::endl
+                << "Gyro  Y [mdps]: " << gyro_y << std::endl
+                << "Gyro  Z [mdps]: " << gyro_z << std::endl
+                << "Accel X   [mg]: " << accel_x << std::endl
+                << "Accel Y   [mg]: " << accel_y << std::endl
+                << "Accel Z   [mg]: " << accel_z << std::endl
+                << std::flush;
       vTaskDelay(pdMS_TO_TICKS(100));
     }
   }
@@ -80,7 +72,7 @@ void mainTask(void *pv)
 extern "C" void app_main(void)
 {
   xEventGroupSensor = xEventGroupCreate();
-  assert(xEventGroupSensor != nullptr);
+  assert(xEventGroupSensor != NULL);
 
   driver::imu::init(xEventGroupSensor, EVENT_GROUP_SENSOR_ENCODER);
   driver::encoder::init(xEventGroupSensor, EVENT_GROUP_SENSOR_IMU);
@@ -89,6 +81,6 @@ extern "C" void app_main(void)
 
   vTaskDelay(pdMS_TO_TICKS(2000));
 
-  xTaskCreatePinnedToCore(sensorTask, "sensorTask", 8192, xTaskGetCurrentTaskHandle(), PRIORITY_HIGH, nullptr, CORE_SENSE);
-  xTaskCreatePinnedToCore(mainTask, "mainTask", 8192, xTaskGetCurrentTaskHandle(), PRIORITY_HIGH, nullptr, CORE_MAIN);
+  xTaskCreatePinnedToCore(sensorTask, "sensorTask", 8192, xTaskGetCurrentTaskHandle(), 10, NULL, 0);
+  xTaskCreatePinnedToCore(mainTask, "mainTask", 8192, xTaskGetCurrentTaskHandle(), 10, NULL, 1);
 }
