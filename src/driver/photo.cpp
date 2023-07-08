ae2c8de03f06e052bb029fdab070ddd94b316046
ypc2e55orj <ypc2e55orj@gmail.com>
2023-07-08 23:20:52 +0900

    環境光を引く

--- src/driver/photo.cpp
+++ src/driver/photo.cpp
@@ -48,9 +48,10 @@ namespace driver::photo
 
   inline static void sampling(uint8_t pos)
   {
+    int ambient = driver::adc::voltage(photo_pins[pos].photo);
     gpio_set_level(photo_pins[pos].ir, 1);
     ets_delay_us(10);
-    result[pos] = driver::adc::voltage(photo_pins[pos].photo);
+    result[pos] = (driver::adc::voltage(photo_pins[pos].photo) - ambient);
     gpio_set_level(photo_pins[pos].ir, 0);
   }
 
