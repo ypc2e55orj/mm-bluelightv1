cb8f35a4dd8147d2cedef1c46a472fb7e2ebacbf
ypc2e55orj <ypc2e55orj@gmail.com>
2023-09-17 10:06:47 +0900

    indent

--- src/driver/adc.h
+++ src/driver/adc.h
@@ -4,10 +4,14 @@
 
 #include <hal/adc_types.h>
 
-namespace driver {
-  class Adc {
+namespace driver
+{
+  class Adc
+  {
   private:
-    class AdcImpl; std::unique_ptr<AdcImpl> impl_;
+    class AdcImpl;
+    std::unique_ptr<AdcImpl> impl_;
+
   public:
     explicit Adc(adc_unit_t unit, adc_channel_t channel);
     ~Adc() = default;
