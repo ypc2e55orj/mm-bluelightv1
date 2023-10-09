bee3f025bfef16c2c846eca811ae27fc53de92bb
ypc2e55orj <ypc2e55orj@gmail.com>
2023-10-09 23:53:26 +0900

    はい

--- src/driver/hardware/buzzer.hpp
+++ src/driver/hardware/buzzer.hpp
@@ -8,41 +8,39 @@
 
 namespace driver::hardware
 {
-  struct BuzzerNote
-  {
-    uint32_t frequency; // [Hz]
-    uint32_t duration;  // [ms]
-  };
-
-  enum class BuzzerMode
-  {
-    None,             // 無音
-    InitilizeFailed,  // 初期化失敗
-    InitilizeSuccess, // 初期化成功
-    Searching,        // 迷路探索中(ループ再生)
-    SearchWarning,    // 迷路探索中警告
-    SearchFailed,     // 迷路探索失敗
-    SearchSuccess,    // 迷路探索成功
-    FastFailed,       // 最短走行失敗
-    FastSuccess,      // 最短走行成功
-    StartRunning,     // 走行開始
-    EndRunning,       // 走行終了
-  };
-
   class Buzzer
   {
-  private:
-    class BuzzerImpl;
-    std::unique_ptr<BuzzerImpl> impl_;
+  public:
+    enum class Mode
+    {
+      None [[maybe_unused]],              // 無音
+      InitializeFailed [[maybe_unused]],  // 初期化失敗
+      InitializeSuccess [[maybe_unused]], // 初期化成功
+      Searching [[maybe_unused]],         // 迷路探索中(ループ再生)
+      SearchWarning [[maybe_unused]],     // 迷路探索中警告
+      SearchFailed [[maybe_unused]],      // 迷路探索失敗
+      SearchSuccess [[maybe_unused]],     // 迷路探索成功
+      FastFailed [[maybe_unused]],        // 最短走行失敗
+      FastSuccess [[maybe_unused]],       // 最短走行成功
+      StartRunning [[maybe_unused]],      // 走行開始
+      EndRunning [[maybe_unused]],        // 走行終了
+      LowBattery [[maybe_unused]],        // バッテリー切れ
+      Select [[maybe_unused]],            // モード選択
+      Ok [[maybe_unused]],                // 確定
+      Cancel [[maybe_unused]],            // キャンセル
+    };
 
   public:
     explicit Buzzer(gpio_num_t buzzer_num);
     ~Buzzer();
 
-    bool start(const uint32_t usStackDepth, UBaseType_t uxPriority, BaseType_t xCoreID);
+    bool start(uint32_t usStackDepth, UBaseType_t uxPriority, BaseType_t xCoreID);
     bool stop(TickType_t xTicksToWait);
 
-    bool set(BuzzerNote &note);
-    bool set(BuzzerMode mode);
+    void set(Mode mode, bool loop);
+
+  private:
+    class BuzzerImpl;
+    std::unique_ptr<BuzzerImpl> impl_;
   };
 }
