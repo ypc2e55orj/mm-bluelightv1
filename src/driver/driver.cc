3a25a3beb59b2a70f03c78048c06f5266cae8c25
ypc2e55orj <ypc2e55orj@gmail.com>
2023-11-13 16:23:57 +0900

    読みやすくするためにインデントを入れた

--- src/driver/driver.cc
+++ src/driver/driver.cc
@@ -5,39 +5,66 @@
 
 namespace driver {
 Driver::Driver() {
-  spi_encoder_ = std::make_unique<peripherals::Spi>(
-      SPI2_HOST, GPIO_NUM_ENCODER_SPI_MISO, GPIO_NUM_ENCODER_SPI_MOSI,
-      GPIO_NUM_ENCODER_SPI_SCLK, 4);
-  spi_imu_ = std::make_unique<peripherals::Spi>(
-      SPI3_HOST, GPIO_NUM_IMU_SPI_MISO, GPIO_NUM_IMU_SPI_MOSI,
-      GPIO_NUM_IMU_SPI_SCLK, 16);
-  buzzer = std::make_unique<hardware::Buzzer>(GPIO_NUM_BUZZER);
-  battery = std::make_unique<hardware::Battery>(ADC_UNIT_BATTERY,
-                                                ADC_CHANNEL_BATTERY);
-  imu = std::make_unique<hardware::Imu>(*spi_imu_, GPIO_NUM_IMU_SPI_CS);
-  encoder_left = std::make_unique<hardware::Encoder>(
-      *spi_encoder_, GPIO_NUM_ENCODER_SPI_CS_LEFT);
-  encoder_right = std::make_unique<hardware::Encoder>(
-      *spi_encoder_, GPIO_NUM_ENCODER_SPI_CS_RIGHT);
-  fs = std::make_unique<hardware::Fs>(10);
-  motor_left = std::make_unique<hardware::Motor>(0, GPIO_NUM_MOTOR_LEFT_IN1,
-                                                 GPIO_NUM_MOTOR_LEFT_IN2);
-  motor_right = std::make_unique<hardware::Motor>(1, GPIO_NUM_MOTOR_RIGHT_IN1,
-                                                  GPIO_NUM_MOTOR_RIGHT_IN2);
-  indicator =
-      std::make_unique<hardware::Indicator>(GPIO_NUM_INDICATOR, NUM_INDICATORS);
+  // clang-format off
+  buzzer = std::make_unique<hardware::Buzzer>(
+      GPIO_NUM_BUZZER);
 
-  hardware::Photo::Config config{};
-  config.adc_unit = ADC_UNIT_PHOTO;
-  config.gpio_num[0] = GPIO_NUM_PHOTO_LEFT90,
-  config.adc_channel[0] = ADC_CHANNEL_PHOTO_LEFT90;
-  config.gpio_num[1] = GPIO_NUM_PHOTO_LEFT45,
-  config.adc_channel[1] = ADC_CHANNEL_PHOTO_LEFT45;
-  config.gpio_num[2] = GPIO_NUM_PHOTO_RIGHT45,
-  config.adc_channel[2] = ADC_CHANNEL_PHOTO_RIGHT45;
-  config.gpio_num[3] = GPIO_NUM_PHOTO_RIGHT90,
-  config.adc_channel[3] = ADC_CHANNEL_PHOTO_RIGHT90;
+  battery = std::make_unique<hardware::Battery>(
+      ADC_UNIT_BATTERY,
+      ADC_CHANNEL_BATTERY);
+
+  spi_imu_ = std::make_unique<peripherals::Spi>(
+    SPI3_HOST,
+    GPIO_NUM_IMU_SPI_MISO,
+    GPIO_NUM_IMU_SPI_MOSI,
+    GPIO_NUM_IMU_SPI_SCLK,
+    16);
+  imu = std::make_unique<hardware::Imu>(
+      *spi_imu_,
+      GPIO_NUM_IMU_SPI_CS);
+
+  spi_encoder_ = std::make_unique<peripherals::Spi>(
+    SPI2_HOST,
+    GPIO_NUM_ENCODER_SPI_MISO,
+    GPIO_NUM_ENCODER_SPI_MOSI,
+    GPIO_NUM_ENCODER_SPI_SCLK,
+    4);
+  encoder_left = std::make_unique<hardware::Encoder>(
+      *spi_encoder_,
+      GPIO_NUM_ENCODER_SPI_CS_LEFT);
+  encoder_right = std::make_unique<hardware::Encoder>(
+      *spi_encoder_,
+      GPIO_NUM_ENCODER_SPI_CS_RIGHT);
+
+  fs = std::make_unique<hardware::Fs>(10);
+
+  motor_left = std::make_unique<hardware::Motor>(
+      0,
+      GPIO_NUM_MOTOR_LEFT_IN1,
+      GPIO_NUM_MOTOR_LEFT_IN2);
+  motor_right = std::make_unique<hardware::Motor>(
+      1,
+      GPIO_NUM_MOTOR_RIGHT_IN1,
+      GPIO_NUM_MOTOR_RIGHT_IN2);
+
+  indicator = std::make_unique<hardware::Indicator>(
+      GPIO_NUM_INDICATOR,
+      NUM_INDICATORS);
+
+  hardware::Photo::Config config{
+      .adc_unit = ADC_UNIT_PHOTO,
+      .adc_channel = {
+          ADC_CHANNEL_PHOTO_LEFT90,
+          ADC_CHANNEL_PHOTO_LEFT45,
+          ADC_CHANNEL_PHOTO_RIGHT45,
+          ADC_CHANNEL_PHOTO_RIGHT90},
+      .gpio_num = {
+          GPIO_NUM_PHOTO_LEFT90,
+          GPIO_NUM_PHOTO_LEFT45,
+          GPIO_NUM_PHOTO_RIGHT45,
+          GPIO_NUM_PHOTO_RIGHT90}};
   photo = std::make_unique<hardware::Photo>(config);
+  // clang-format on
 }
 Driver::~Driver() = default;
 }  // namespace driver
--- src/driver/hardware/photo.h
+++ src/driver/hardware/photo.h
@@ -27,8 +27,8 @@ class Photo final : DriverBase {
 
   struct Config {
     adc_unit_t adc_unit;
-    gpio_num_t gpio_num[4];
     adc_channel_t adc_channel[4];
+    gpio_num_t gpio_num[4];
   };
 
   explicit Photo(Config &config);
